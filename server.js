dc5c7f2921247c00eaa992baf6d316917671126c
MadEyE355 <moksh9355472532@gmail.com>
2024-11-15 14:48:30 +0530

    fixed db connection

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ const io = new Server(server);
 app.use(express.json());
 
 const { MongoClient } = require('mongodb');
-// const uri = "mongodb://localhost:27017/"; // replace with your actual connection string
-require('dotenv').config();
-const uri = process.env.MONGODB_URI;
+const uri = "mongodb+srv://MadEyE:mongodb1@cluster0.83apq.mongodb.net/"; // replace with your actual connection string
+// require('dotenv').config();
+// const uri = process.env.MONGODB_URI;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
